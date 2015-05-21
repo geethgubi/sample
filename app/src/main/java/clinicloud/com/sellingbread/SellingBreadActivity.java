30057da355813f7476bf5062ebeee7794588d1d5
Geetha Gubendran <geetha.gubendran@clinicloud.com>
2015-05-21 15:39:33 +1000

    finished for 1st release

--- app/src/main/java/clinicloud/com/sellingbread/SellingBreadActivity.java
+++ app/src/main/java/clinicloud/com/sellingbread/SellingBreadActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class SellingBreadActivity extends ActionBarActivity {
@@ -12,6 +13,8 @@ public class SellingBreadActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selling_bread);
+
+        TextView txtView = (TextView)findViewById(R.id.text);
     }
 
     @Override
