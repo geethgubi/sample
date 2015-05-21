7a184571fcc09b3208be407477f61d11c51cc365
Geetha Gubendran <geetha.gubendran@clinicloud.com>
2015-05-21 16:57:32 +1000

    Checking pull command

---
+++ app/src/main/java/clinicloud/com/sellingbread/Simple.java
@@ -0,0 +1,10 @@
+package clinicloud.com.sellingbread;
+
+/**
+ * Created by geetha on 21/05/15.
+ */
+public class Simple {
+    public static void main(String args[]) {
+        System.out.println("*****Hello Hello");
+    }
+}
---
+++ app/src/main/java/clinicloud/com/sellingbread/Test.java
@@ -0,0 +1,7 @@
+package clinicloud.com.sellingbread;
+
+/**
+ * Created by geetha on 21/05/15.
+ */
+public class Test {
+}
